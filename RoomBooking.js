53e4ae8790df7d6d06a311e7c23f32fbf26d5051
alia8858 <130959179+alia8858@users.noreply.github.com>
2024-11-06 11:36:13 +0200

    Create RoomBooking.js

---
+++ RoomBooking.js
@@ -0,0 +1,61 @@
+1. Standard Room
+
+Size: 30 square meters
+
+Amenities:
+
+- Comfortable double bed
+- Spacious wardrobe
+- Private bathroom
+- Air conditioner
+- Flat-screen TV
+- Free Wifi
+
+Price: $100 per night
+
+Rooms are 1-60
+
+2. Deluxe Room
+
+Size: 40 square meters
+
+Amenities: 
+
+- Comfortable double bed
+- Spacious wardrobe
+- Private bathroom
+- Air conditioner
+- Flat-screen TV
+- Free Wifi
+- Electric kettle
+- Mini refrigerator stocked with refreshments
+- Hair dryer
+
+Price: $150 per night
+
+Rooms are 61-90
+
+
+3. Luxe Room
+
+Size: 50 square meters
+
+Amenities:
+
+- Comfortable double bed
+- Spacious wardrobe
+- Private bathroom
+- Air conditioner
+- Flat-screen TV
+- Free Wifi
+- Electric kettle
+- Mini refrigerator stocked with refreshments
+- Hair dryer
+- Breakfast included
+- Private balcony offering panoramic views of the mountains and sea
+- Elegant modern furniture
+
+
+Price: $200 per night
+
+Rooms are 91-100
